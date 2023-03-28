4d76355f245d7829a6ee8ea524ab8ceef15eafec
prez2307 <86853355+prez2307@users.noreply.github.com>
2023-03-28 17:30:18 -0400

    Time: 1961 ms (63.79%), Space: 60.2 MB (56.90%) - LeetHub

--- 2106-maximum-fruits-harvested-after-at-most-k-steps/2106-maximum-fruits-harvested-after-at-most-k-steps.py
+++ 2106-maximum-fruits-harvested-after-at-most-k-steps/2106-maximum-fruits-harvested-after-at-most-k-steps.py
@@ -1,26 +1,27 @@
 class Solution:
     def maxTotalFruits(self, fruits: List[List[int]], startPos: int, k: int) -> int:
         prefix_sum = [0]
-        position = []
+        positions = []
         num_fruits = []
         for pos, count in fruits:
             prefix_sum.append(prefix_sum[-1] + count)
-            position.append(pos)
+            positions.append(pos)
             num_fruits.append(count)
-
+            
         maximum = 0
-
-        for i, position in enumerate(position):
-            remaining = k - abs(startPos - position)
+        for i, pos in enumerate(positions):
+            remaining = k - abs(startPos - pos)
             if remaining >= 0:
-                if position < startPos:
-                    temp = max(position + remaining, startPos)
-                    j = bisect_left(fruits, [temp, inf])
-                    if j == len(fruits) or fruits[j][0] > temp:
+                if pos < startPos:
+                    temp = max(pos + remaining, startPos)
+                    j = bisect_left(fruits, [temp, float('inf')])
+                    if j == len(fruits):
+                        j -= 1
+                    elif fruits[j][0] > temp:
                         j -= 1
                     maximum = max(maximum, prefix_sum[j + 1] - prefix_sum[i])
                 else:
-                    temp = min(position - remaining, startPos)
+                    temp = min(pos - remaining, startPos)
                     j = bisect_left(fruits, [temp, 0])
                     if fruits[j][0] < temp:
                         j += 1
