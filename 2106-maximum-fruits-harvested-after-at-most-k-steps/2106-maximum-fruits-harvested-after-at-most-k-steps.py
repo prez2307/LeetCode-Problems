c9e02d732fa90f2a140fc4a26bbd7a0309a56918
prez2307 <86853355+prez2307@users.noreply.github.com>
2023-03-28 17:38:15 -0400

    Time: 2000 ms (58.62%), Space: 59.9 MB (91.38%) - LeetHub

--- 2106-maximum-fruits-harvested-after-at-most-k-steps/2106-maximum-fruits-harvested-after-at-most-k-steps.py
+++ 2106-maximum-fruits-harvested-after-at-most-k-steps/2106-maximum-fruits-harvested-after-at-most-k-steps.py
@@ -14,7 +14,7 @@ class Solution:
             if remaining >= 0:
                 if pos < startPos:
                     temp = max(pos + remaining, startPos)
-                    j = bisect_left(fruits, [temp, float('inf')])
+                    j = bisect_right(fruits, [temp, float('inf')])
                     if j == len(fruits):
                         j -= 1
                     elif fruits[j][0] > temp:
@@ -22,7 +22,7 @@ class Solution:
                     maximum = max(maximum, prefix_sum[j + 1] - prefix_sum[i])
                 else:
                     temp = min(pos - remaining, startPos)
-                    j = bisect_right(fruits, [temp, 0])
+                    j = bisect_left(fruits, [temp, 0])
                     if fruits[j][0] < temp:
                         j += 1
                     maximum = max(maximum, prefix_sum[i + 1] - prefix_sum[j])
